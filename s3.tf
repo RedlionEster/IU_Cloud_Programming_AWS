a94a154f3f34b39521e7b5b67095a2208fb2d677
Ester <egjorek@gmail.com>
2024-11-18 12:00:44 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,6 @@
 # Create an S3 bucket for static website hosting
 resource "aws_s3_bucket" "site_bucket" {
   bucket = "${var.project_name}-bucket-${random_id.bucket_id.hex}"
-  acl    = "private"
 
   tags = {
     Name = var.project_name
@@ -23,12 +22,12 @@ resource "aws_s3_bucket_website_configuration" "site_bucket_website" {
 }
 
 # Upload website files
-resource "aws_s3_bucket_object" "site_files" {
+resource "aws_s3_object" "site_files" {
   for_each = fileset("${path.module}/website", "**/*")
 
-  bucket = aws_s3_bucket.site_bucket.id
-  key    = each.value
-  source = "${path.module}/website/${each.value}"
+  bucket       = aws_s3_bucket.site_bucket.id
+  key          = each.value
+  source       = "${path.module}/website/${each.value}"
   content_type = lookup(var.mime_types, split(".", each.value)[length(split(".", each.value)) - 1], "application/octet-stream")
 }
 
